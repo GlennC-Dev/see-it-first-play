6991b06183193278270f6772eacd454c2f96330f
GlennC-Dev <acq.yuemari2@gmail.com>
2026-06-06 22:59:53 +0800

    Adjust image position in Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,7 +13,7 @@ const Hero = ({ onOpenChat }: { onOpenChat: () => void }) => {
           src={glennProfile.url}
           alt="Glenn Charifa"
           className="absolute inset-0 w-full h-full"
-          style={{ objectFit: "cover", objectPosition: "center top" }}
+          style={{ objectFit: "cover", objectPosition: "35% 15%" }}
         />
         <div
           className="absolute inset-0"
